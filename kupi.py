5f75ac934ebf1a7056b3b505822fcea4bd9cc91b
Я <veronika.havlova@mensagymnazium.cz>
2024-12-26 11:36:14 +0100

    add images, rebuild files

---
+++ kupi.py
@@ -0,0 +1,46 @@
+'''
+TODO: WRITE OWN PARSER!!!
+The pykupi parser im using does not provide access to drink amount
+eg. 6 bottles = higher price -> I am filtering those offers out;
+This file will (eventually, hopefully) get a big rewrite
+'''
+
+import asyncio
+from parser import KupiParser
+from models import session, Drink, Store
+
+
+async def update_prices():
+    parser = KupiParser()
+    drinks = session.query(Drink).all()
+    stores = session.query(Store).all()
+    stores_table = {store.name: store for store in stores}
+
+    try:
+        for drink in drinks:
+            raw = await parser.get_prices(drink.name)
+            # to filter out offers from obscure stores nobody knows
+            # very ugly line I know
+            filtered_offers = [offer for offer in raw.offers if offer.offered_by.lower() in stores_table]
+
+            if filtered_offers:
+                lowest_cost = filtered_offers[0].price
+                if lowest_cost < drink.normal_cost:
+                    drink.discount = True
+                    drink.discount_cost = lowest_cost
+                    display_store_name = filtered_offers[0].offered_by.lower()
+                    drink.store = stores_table[display_store_name]
+                else:
+                    drink.discount = False
+                    drink.discount_cost = 0
+        session.commit()
+    finally:
+        await parser.session.close()
+
+# necessary for vercel to work?? I copypasted this
+def handler(event, context):
+    asyncio.run(update_prices())
+    return {"status": "Update completed"}
+
+if __name__ == "__main__":
+    asyncio.run(update_prices())
---
+++ models.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table, create_engine, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from config import DATABASE_URI
+
+
+Base = declarative_base()
+
+class Store(Base):
+    __tablename__ = 'stores'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(500), nullable=False, unique=True)
+    display_name = Column(String(500), nullable=False)
+    logo_url = Column(String(500), nullable=False)
+
+
+class Drink(Base):
+    __tablename__ = 'drinks'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(500), nullable=False, unique=True)
+    display_name = Column(String(500), nullable=False, unique=True)
+    normal_cost = Column(Float, nullable=False)
+    discount_cost = Column(Float, nullable=False)
+    image_url = Column(String(200))
+    is_zero = Column(Boolean, default=False)
+    discount = Column(Boolean, default=False)
+
+    store_id = Column(Integer, ForeignKey('stores.id'))
+    store = relationship('Store')
+
+
+engine = create_engine(DATABASE_URI)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
